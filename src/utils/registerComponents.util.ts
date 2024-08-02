d421a4ecd5409baf176622313b9d6c8cc222fb51
zainkurnia_ <programmer-fe3@qwertysystem.net>
2024-08-02 09:21:41 +0700

    initialize new sample project

---
+++ src/utils/registerComponents.util.ts
@@ -0,0 +1,10 @@
+import { App } from 'vue';
+
+import '@/assets/css/main.css';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const registerComponents = (app: App): void => {
+  //
+};
+
+export default registerComponents;
